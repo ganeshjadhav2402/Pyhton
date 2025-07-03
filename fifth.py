83e514b6594798958d63e252d7e0d5be0fe98bf4
ganeshjadhav2402 <ganeshsj2410@gmail.com>
2025-07-03 12:28:04 +0530

    range function

--- fifth.py
+++ fifth.py
@@ -130,6 +130,83 @@ while i <= 10:
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
+# for Loops
+# Loops are used for sequential traversal. For travesing list, string, tuples etc. 
+"""
+nums = [1, 2, 3, 4, 5]
+
+for val in nums:
+    print(val) 
+"""
+    
+"""
+fruits = ["banana", "litchi", "aaple", "mosambi", "orange"]
+
+for val in fruits:
+    print(val)
+"""
+
+"""
+tup = (1, 2, 3, 5, 6, 9, 7)
+
+for num in tup:
+    print(num)
+"""
+
+"""
+str = "GaneshJadhav"
+
+for char in str:
+    if(char == "h"):
+        print("h found")
+        break
+    print(char)
+
+print("Loop END")
+"""
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# Let's Practice 
+# using for
+# ◑ Print the elements of the follweing list using a loop:
+    # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+"""
+num = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+for val in num:
+    print(val)
+"""
+
+# ◑ Search for a number x in this tuple using loop:
+"""
+num = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 49)
+x = int(input("Enter Search Number: "))
+
+idx = 0
+for el in num:
+    if(el == x):
+        print("number found at idx", idx)
+        break
+    idx += 1
+"""
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# range()
+# Range functions returns a sequence of numbers, starting from 0 by default, and increments by
+# 1 (by default), and stops before a specified number.
+
+# range(start?, stop, step?)  ◑" ? ":means optional vlaue
+
+
+
+
+
+
+
+
+
 
 
 
