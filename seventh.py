44920be8f34d26479c443733b001e988de962262
ganeshjadhav2402 <ganeshsj2410@gmail.com>
2025-07-08 10:29:07 +0530

    find word line  usinf while function

--- seventh.py
+++ seventh.py
@@ -74,307 +74,78 @@ os.remove("sample.txt")
 
 # Let‘s Practice
 # Create a new file “practice.txt” using python. Add the following data in it:
+"""
 with open("practice.txt", "w") as f:
     f.write("Hi everyone\nwe are learning File I/O ")
     f.write("using Java. \nI like programming in Java.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
+
+# WAF that replace all occurrences of “java” with “python” in above file.
+"""
+with open("practice.txt", "r") as f:
+    data = f.read()
+    
+new_data = data.replace("Java", "Python")
+print(new_data)
+
+with open("practice.txt", "w") as f:
+    f.write(new_data)
+"""
+
+# Search if the word “learning” exists in the file or not.
+"""
+# using Function 
+def check_word():
+    word = "xlearning"
+    with open("practice.txt", "r") as f:
+        data = f.read()
+        if(word in data):
+            print("FOND")
+        else:
+            print("NOT FOUND")
+
+check_word()
+"""
+
+# using without Function
+"""
+word = "xlearning"
+with open("practice.txt", "r") as f:
+    data = f.read()
+    if(data.find(word) != -1):
+        print("FOND")
+    else:
+        print("NOT FOUND")
+"""
+
+# WAF to find in which line of the file does the word “learning”occur first.
+# Print -1 if word not found.
+# using Function 
+def check_for_word():
+    word = "learning"
+    with open("practice.txt", "r") as f:
+        data = f.read()
+        if(word in data):
+            print("FOND")
+        else:
+            print("NOT FOUND")
+
+def check_for_line():
+    word = "PYQ"
+    data = True
+    line_no = 1
+    with open("practice.txt", "r") as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print(line_no)
+                return
+            line_no += 1
+
+    return -1
+
+print(check_for_line())
+
+# From a file containing numbers separated by comma, print the count of even numbers.
 
 
