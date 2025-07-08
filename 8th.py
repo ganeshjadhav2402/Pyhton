27e75490526d9f8a0c898fde52449e1954b98a77
ganeshjadhav2402 <ganeshsj2410@gmail.com>
2025-07-08 12:09:23 +0530

    8th chapter

---
+++ 8th.py
@@ -0,0 +1,60 @@
+# OOP in Python
+# To map with real world scenarios, we started using objects in code.
+# This is called object oriented programming.
+
+# Class & Object in Python
+
+
+#creating class
+class Students:
+    name = "Kran Kumar"
+    def __init__(self):
+        print(self)
+        print("adding new students in Database...")
+    
+# creating objects
+s1 = Students()
+# print(s1.name)
+
+# s2 = Students()
+# print(s2.name)
+
+
+"""
+class Car:
+    brand = "BMW"
+    color = "Blue"
+    model = "BMW i5 M60 xDrive"
+
+c1 = Car()
+print("Color of Car :",c1.color)
+print("Brand of Car :",c1.brand)
+print("Model of Car :",c1.model)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
