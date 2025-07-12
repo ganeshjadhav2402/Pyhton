fcbf65304443475c3f461dbb804fb72e88cf4cbf
ganeshjadhav2402 <ganeshsj2410@gmail.com>
2025-07-12 10:53:50 +0530

    bank account debit or credit method

--- 8th.py
+++ 8th.py
@@ -161,8 +161,27 @@ class Account:
     def __init__(self, bal, acc):
         self.balance = bal
         self.account_no = acc
+        
+    #debit method
+    def debit(self, amount):
+        self.balance -= amount
+        print("Rs. ", amount, "was debited")
+        print("total balance = ", self.get_balance())
+    
+    #debit method
+    def credit(self, amount):
+        self.balance += amount
+        print("Rs. ", amount, "was credited")
+        print("total balance = ", self.get_balance())
+    
+    def get_balance(self):
+        return self.balance
+    
+acc1 = Account(10000, 658974235)
+acc1.debit(1000)
+acc1.credit(500)
+acc1.credit(40000)
 
-acc1 = Account(100000, 658974235)
 print("Account Balance :",acc1.balance)
 print("Account Number  :",acc1.account_no)
 
