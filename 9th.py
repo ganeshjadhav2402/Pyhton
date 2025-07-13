4cee6822afd3e94181f80fb7dc03c07a324c263b
ganeshjadhav2402 <ganeshsj2410@gmail.com>
2025-07-13 11:10:56 +0530

    private attribute

--- 9th.py
+++ 9th.py
@@ -13,17 +13,20 @@ del s1.name
 print(s1.name)
 """
 
-
+# Private(like) attributes & methods
+# Conceptual Implementations in Python
+# Private attributes & methods ate meant to be used only within the class and are not accessible from outside the class.
 class Account:
     def __init__(self, acc_no, acc_pass):
         self.acc_no = acc_no
         self.__acc_pass = acc_pass # "__"means this is private
         
-    
+    def reset_pass(self):
+        print(self.__acc_pass)
         
 acc1 = Account("65489723", "GSJ21")
 print(acc1.acc_no)
-print(acc1.__acc_pass)
+print(acc1.reset_pass())
 
 
 
