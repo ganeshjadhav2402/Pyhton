34963a02cd70194665a09d5837ed4f713b65aec8
ganeshjadhav2402 <ganeshsj2410@gmail.com>
2025-07-13 10:42:09 +0530

    del keywords

---
+++ 9th.py
@@ -0,0 +1,30 @@
+# del keyword
+# Used to delete object properties or object itself
+# del s1.name
+# del s1
+"""
+class Student:
+    def __init__(self,name):
+        self.name = name
+
+s1 = Student("ganesh")
+print(s1.name)
+del s1.name
+print(s1.name)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
